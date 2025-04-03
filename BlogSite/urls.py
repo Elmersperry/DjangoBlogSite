5a473d90adda84f7047fdddf0d8293a768f0dc83
Elmersperry <Elmersperry@mail.ru>
2025-04-03 21:36:44 +0300

    sdjkfukerfhjukernhbgkhj

--- BlogSite/urls.py
+++ BlogSite/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from blog.views import index, about
+from BlogSite import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    # path('about/', about, name='about'),
-    # path('', index, name='index'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -22,12 +22,13 @@ def add_post(request):
         return render(request, template_name='blog/post_add.html', context=context)
 
     if request.method == "POST":
-        post_form = PostForm(data=request.POST)
+        post_form = PostForm(data=request.POST, files=request.FILES)
         if post_form.is_valid():
             post = Post()
             post.title = post_form.cleaned_data['title']
             post.text = post_form.cleaned_data['text']
             post.author = post_form.cleaned_data['author']
+            post.image = post_form.cleaned_data['image']
             post.save()
             return index(request)
 
@@ -35,3 +36,21 @@ def read_post(request, pk):
     post = Post.objects.get(pk=pk)
     context = {"title": "Информация о посте", "post": post}
     return render(request, template_name="blog/post_detail.html", context=context)
+
+def update_post(request, pk):
+    post = Post.objects.get(pk=pk)
+    if request.method == "POST":
+        post_form = PostForm(data = request.POST, files = request.FILES)
+        if post_form.is_valid():
+            post.title = post_form.cleaned_data['title']
+            post.text = post_form.cleaned_data['text']
+            post.author = post_form.cleaned_data['author']
+            post.image = post_form.cleaned_data['image']
+            return read_post(request, pk = post.id)
+        else:
+            post_form = PostForm(initial = {
+                "title": post.title,
+                "author": post.author,
+                "text": post.text
+            })
+            return render(request, template_name="blog/post_edit.html", context = {"form": post_form})
